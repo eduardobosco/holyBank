b770e228de5bf6137908ae402300ee5a1b3b88fd
eduardobosco <eduardoboscocs@gmail.com>
2022-08-09 21:53:11 -0300

    commit inicial projeto

---
+++ src/main/java/br/com/holy/bank/HolyBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.holy.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyBankApplication.class, args);
+	}
+
+}
